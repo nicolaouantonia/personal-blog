b6c71587ebb19045168cf29783f98a24323b7d38
Antonia Nikolaou <antonini@amdocs.com>
2022-10-13 22:48:56 +0300

    New Article University Years

--- src/routes/posts/2020-03-29-HowIManagedFullTimeWorkAndMaster/post.ts
+++ src/routes/posts/2020-03-29-HowIManagedFullTimeWorkAndMaster/post.ts
@@ -1,6 +1,6 @@
 export default {
   title: `How I Managed Full Time Job & Master's Degree`,
-  tags: ['myexperiences'],
+  tags: ['myexperiences', 'universitylife'],
   spoiler: 'Studying for a master’s degree while working a full-time job was incredibly challenging. Learn how I managed to achieve my goal and get my degree with ‘Excellent’ grade.',
   getContent: () => import('./document.mdx'),
 }
