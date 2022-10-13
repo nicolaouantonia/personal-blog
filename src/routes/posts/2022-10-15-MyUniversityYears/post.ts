1d3dec1a4cf0ce69253a45b8137a2accba76ecc7
Antonia Nikolaou <antonini@amdocs.com>
2022-10-13 22:49:23 +0300

    New Article University Years

---
+++ src/routes/posts/2022-10-15-MyUniversityYears/post.ts
@@ -0,0 +1,6 @@
+export default {
+  title: `My University years`,
+  tags: ['myexperiences', 'universitylife'],
+  spoiler: `In this article, I’m answering some of the most common questions regarding my University years.`,
+  getContent: () => import('./document.mdx'),
+}
