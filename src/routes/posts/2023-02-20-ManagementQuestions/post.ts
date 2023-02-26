5219de67282c3732740c6a9af49a0205ecf408ab
Antonia <nicolaouantonia@gmail.com>
2023-02-26 18:41:01 +0200

    add management post

---
+++ src/routes/posts/2023-02-20-ManagementQuestions/post.ts
@@ -0,0 +1,6 @@
+export default {
+  title: `Management/People Management Common questions`,
+  tags: ['myexperiences', 'leading'],
+  spoiler: `In this article, I’m answering some of the most common questions regarding my experiences in management and people management after 2 years in a team leader position.`,
+  getContent: () => import('./document.mdx'),
+}
